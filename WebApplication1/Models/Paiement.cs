9aa33f1292a15756bbccab2841f89aadb9330b0b
Umermohammed7 <umer.mohammed57@gmail.com>
2025-01-14 14:54:01 +0100

    Paiement Terminé

--- WebApplication1/Models/Paiement.cs
+++ WebApplication1/Models/Paiement.cs
@@ -2,5 +2,14 @@
 {
     public class Paiement
     {
+        private int id { get; }
+        private DateTime date;
+        private double Montant_total { get; set; }
+        private string detail { get; set; }
+
+        public Paiement()
+        {
+
+        }
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f789cb213fce64d84563a7af555fb8bb2d629a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45cb0ed6d4e0b966efd2bf6feacbe4185eccf86")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
