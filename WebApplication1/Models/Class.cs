f1d5236f4a2bb4cf76c0d527077da7afe86713ea
Brigoo <a.brigouleix@gmail.com>
2025-01-14 14:57:01 +0100

    produit class

---
+++ WebApplication1/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Produit 
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Libelle { get; set; }
+
+        public Produit() { }
+
+    }
+}
